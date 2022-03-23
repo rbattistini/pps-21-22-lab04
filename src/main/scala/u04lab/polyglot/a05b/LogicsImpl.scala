b46ebfc6f6da5460f65b1415d1cfd3288a6462f8
Riccardo Battistini <riccardo.battistini@protonmail.com>
2022-03-23 13:47:31 +0100

    Update exercise 3

--- src/main/scala/u04lab/polyglot/a05b/LogicsImpl.scala
+++ src/main/scala/u04lab/polyglot/a05b/LogicsImpl.scala
@@ -11,16 +11,23 @@ class LogicsImpl(private val gridSize: Int) extends Logics:
     Tup2(random.nextInt(gridSize - 2) + 1, random.nextInt(gridSize - 2) + 1)
   private var tickCount = 0
 
+  def onSameAxis(x: Int, y: Int): Boolean = x == y
+  def isInRange(x: Int, y: Int, offset: Int): Boolean = math.abs(x -y) <= offset
+
+  def onXAxis(t1: Tup2[Int, Int], t2: Tup2[Int, Int], offset: Int): Boolean =
+    onSameAxis(t1.x, t2.x) && isInRange(t1.y, t2.y, offset)
+  def onYAxis(t1: Tup2[Int, Int], t2: Tup2[Int, Int], offset: Int): Boolean =
+    onSameAxis(t1.y, t2.y) && isInRange(t1.x, t2.x, offset)
+  def onDiagonal(t1: Tup2[Int, Int], t2: Tup2[Int, Int], offset: Int): Boolean =
+    t1.x - t1.y == t2.x - t2.y && isInRange(t1.x, t2.x, offset) ||
+    t1.x + t1.y == t2.x + t2.y && isInRange(t1.x, t2.x, offset)
+
   def isTupleAnElement(t1: Tup2[Int, Int], t2: Tup2[Int, Int], offset: Int): Boolean = (t1, t2) match
-    case (t1, t2) if t1.x == t2.x && math.abs(t1.y - t2.y) <= offset |
-      t1.y == t2.y && math.abs(t1.x - t2.x) <= offset |
-      t1.x - t1.y == t2.x - t2.y && math.abs(t1.x - t2.x) <= offset |
-      t1.x + t1.y == t2.x + t2.y && math.abs(t1.x - t2.x) <= offset => true
+    case (t1, t2) if  onXAxis(t1, t2, offset) || onYAxis(t1, t2, offset) || onDiagonal(t1, t2, offset) => true
     case _ => false
 
   def isTupleOutsideGrid(t: Tup2[Int, Int], offset: Int, gridSize: Int): Boolean = t match
-    case t if t.y - offset < 0 | t.y + offset >= gridSize |
-      t.x - offset < 0 | t.x + offset > gridSize => true
+    case t if t.y - offset < 0 || t.y + offset >= gridSize || t.x - offset < 0 || t.x + offset > gridSize => true
     case _ => false
 
   override def tick(): Unit = {tickCount = tickCount + 1}
